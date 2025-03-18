cc807e283502d4ee271f1aa2f2c93d8aaec9054e
adrianmorfin10 <adri_mo95@hotmail.com>
2025-03-18 14:50:07 -0600

    m3

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -52,7 +52,7 @@ const projects: Project[] = [
 ];
 
 export default function Projects() {
-  // Inicializamos los controles de animación dentro del componente
+  // Inicializamos los controles de animación fuera del loop
   const controlsArray = projects.map(() => useAnimation());
 
   // Referencias para los proyectos
