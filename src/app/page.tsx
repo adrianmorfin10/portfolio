e5d3c1903c99ec1677504e5dd8eb56b14fc5aca3
adrianmorfin10 <adri_mo95@hotmail.com>
2025-05-16 13:24:43 -0600

    ajuds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,12 @@ import Projects from './components/projects';
 import Footer from './components/footer';
 import Section from './components/section';
 import Tools from './components/tools';
-import Services from './components/services'; // <-- Nuevo componente
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Section />
-  
       <Tools />
       <Projects />
       <Footer />
