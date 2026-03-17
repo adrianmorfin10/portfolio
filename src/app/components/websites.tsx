d442320b4d597e93002585c6652bb1e636d481c5
adrianmorfin10 <adri_mo95@hotmail.com>
2026-03-17 17:06:29 -0600

    PORTA

--- src/app/components/websites.tsx
+++ src/app/components/websites.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { useState, useRef, useCallback, useEffect } from "react";
+import { useState, useRef, useCallback } from "react";
 import Image from "next/image";
 
 const websites = [
